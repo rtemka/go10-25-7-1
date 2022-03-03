7206a3560f62ba449ba5790d72aff58c02b67c02
Artem Rybakov <gizmo3000@yandex.ru>
2022-03-03 16:44:59 +0300

    fix print messages

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-	fmt.Print("Введите целое число: ")
+	fmt.Print("Введите данные: ")
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		txt := scanner.Text()
-		fmt.Printf("Вы ввели число: %s\n", txt)
+		fmt.Printf("Вы ввели следующие данные: %s\n", txt)
 	}
 }
